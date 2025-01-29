ce3bada3ba548530967584ed9fd71a6c6e6f7769
Sharpeim <SharpHaiyan@gmail.com>
2025-01-29 10:42:32 -0500

    Fix base URL and SVG ref in script.min.js

--- src/js/main/anchor.js
+++ src/js/main/anchor.js
@@ -2,8 +2,9 @@
   var headings = document.querySelectorAll('h1[id],h2[id],h3[id],h4[id],h5[id],h6[id]');
   for (var i = 0; i < headings.length; i++) {
     var img = document.createElement('img');
-    img.setAttribute('src', '/assets/img/link-symbol.svg');
-
+    var baseUrl = document.querySelector('body').getAttribute('data-baseurl') || "";
+    img.setAttribute('src', baseUrl + '/assets/img/link-symbol.svg');
+    
     var a = document.createElement('a');
     a.setAttribute('href', '#' + headings[i].getAttribute('id'));
     a.classList.add('anchor');
